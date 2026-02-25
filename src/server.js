02701ea5dc4c232cd35b6572505ac6c019991da7
Leon Bryan <leonard.bryan.email@gmail.com>
2026-02-25 16:54:11 +0800

    Added few more shop entries for testing purposes

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`\nServer running on http://localhost:${PORT}\nTo test, navigate to the route /shops\nHappy coding.\n`)
+    console.log(`\nServer running on http://localhost:${PORT}\nTo test, navigate to the route http://localhost:${PORT}/shops\nHappy coding.\n`)
 })
