e862710bc6abfa7d4ce671ddb1df3d074d2962db
Leon Bryan <leonard.bryan.email@gmail.com>
2026-02-27 15:18:21 +0800

    Successfully returned the data from Supabase

--- src/controllers/shopsController.js
+++ src/controllers/shopsController.js
@@ -1,10 +1,30 @@
 import shopsJson from '../mock_data.json' with {type: 'json'}
+import supabase from '../config/supabase.js';
 
 export const getShops = async (req, res) => {
 
     const { municipality, minRating } = req.query;
 
-    const shops = shopsJson;
+    let query = supabase.from('shops').select('*');
+
+    if (municipality) {
+        query = query.eq('municipality', municipality);
+    }
+
+    if (minRating) {
+        query = query.gte('rating', parseFloat(minRating));
+    }
+
+    const { data, error } = await query;
+
+    if (error) {
+        return res.status(500).json({ Error: error.message });
+    }
+
+    res.json(data);
+
+    /*
+    DEV ENVIRONMENT TESTING - LOCAL JSON FILE
 
     let filtered = shops;
 
@@ -20,4 +40,5 @@ export const getShops = async (req, res) => {
     }
 
     res.json(filtered);
+    */
 }
