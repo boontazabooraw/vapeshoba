0c5a0209ab3984977f4fb0c72ef28f916efee7d3
Leon Bryan <leonard.bryan.email@gmail.com>
2026-03-06 03:28:01 +0800

    Fixed env import on server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import app from "./app.js";
-import { loadEnvFile } from 'node:process';
+import dotenv from 'dotenv';
 
-loadEnvFile();
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    if (!process.env.SUPABASE_URL) {
+    if (!supabaseUrl) {
         console.error('Error: DB URL is Missing')
     }
-    if (!process.env.SUPABASE_KEY) {
+    if (!supabaseKey) {
         console.error('Error: DB KEY is Missing')
     }
-    if (process.env.SUPABASE_KEY && process.env.SUPABASE_URL) {
+    if (supabaseUrl && supabaseKey) {
         console.log('\nEnvironment variables are all set.')
     }
     console.log(`\nServer running on http://localhost:${PORT}\nTo test, navigate to the route http://localhost:${PORT}/shops\nHappy coding.\n`)
