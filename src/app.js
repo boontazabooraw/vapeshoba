8f47cb24a8be6e728c7b87f63447cb2aeaabe04c
Leon Bryan <leonard.bryan.email@gmail.com>
2026-03-17 13:55:22 +0800

    FEAT: Added rate limiter for production

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import router from './routes/shops.js';
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-//Allow all domains and selected methods
+// Allow all domains and selected methods
 app.use(cors({
     origin: "*",
     methods: ["GET"],
@@ -12,6 +16,20 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+// Rate Limiting in production
+if (process.env.NODE_ENV === 'production') {
+    const limiter = rateLimit({
+        windowMs: 15 * 6 * 1000,
+        max: 50,
+        statusCode: 429,
+        standardHeaders: true,
+        legacyHeaders: false,
+        message: { error: `Error ${statusCode}: Woah woah woah slow down buddy...` }
+    });
+    app.use(limiter);
+}
+
 app.use('/', router)
 
 export default app;
